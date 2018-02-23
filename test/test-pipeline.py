62ecb67817d0a2a70835d649c9255ae9e9096db6
joezuntz <joezuntz@googlemail.com>
2018-02-23 07:03:14 -0800

    Add cori yaml file for execution

--- test/test-pipeline.py
+++ test/test-pipeline.py
@@ -3,10 +3,10 @@ import pipette_lib.wl_apps
 import pathlib
 import os
 import yaml
+import sys
 
-
-def test_pipeline():
-    config = yaml.load(open("./test/config.yml"))
+def test_pipeline(config_filename="./test/config.yml"):
+    config = yaml.load(open(config_filename))
     # Required configuration information
 
     # List of stage names, must be imported somewhere
@@ -25,4 +25,9 @@ def test_pipeline():
 
 
 if __name__ == '__main__':
-    test_pipeline()
+    narg = len(sys.argv)
+    if narg>1:
+        test_pipeline(sys.argv[1])
+    else:
+        test_pipeline()
+
