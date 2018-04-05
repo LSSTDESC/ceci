b7ea3b44f53a0816b873537338966b988204a5c2
EiffL <fr.eiffel@gmail.com>
2018-04-05 01:19:07 -0400

    Adds functionalities to push module to ceci-central

--- ceci/main.py
+++ ceci/main.py
@@ -5,6 +5,10 @@ import sys
 import parsl
 import argparse
 
+CECI_CENTRAL_USER='EiffL'
+CECI_CENTRAL_REPO=f'git@github.com:{CECI_CENTRAL_USER}/ceci-central.git'
+CECI_CENTRAL_DOCKER=f'{CECI_CENTRAL_USER}/ceci-base'
+
 # Add the current dir to the path - often very useful
 sys.path.append(os.getcwd())
 
@@ -82,12 +86,71 @@ def export_cwl(args):
     cwl_wf = pipeline.generate_cwl(inputs)
     cwl_wf.export(f'{path}/pipeline.cwl')
 
+def push_module(args):
+    """
+    This function exports the definition of a module as CWL files along with a
+    Dockerfile image
+    """
+    import git
+    import tempfile
+    import datetime
+
+    module="ceci_example"
+
+    # First introspect the current git repository to extract some info
+    r = git.Repo(search_parent_directories=True)
+    config = r.config_reader()
+    user = config.get_value("user","name")
+    email = config.get_value("user","email")
+    remote = next(r.remote().urls)
+    hexsha = r.head.object.hexsha
+
+    # Now create a temporary folder to do the rest of the work
+    with tempfile.TemporaryDirectory() as tmpdir:
+
+        print("Retrieving local copy of ceci-central")
+        repo = git.Repo.clone_from(CECI_CENTRAL_REPO, tmpdir)
+        # Checkout branch associated with module
+        repo.git.checkout(f'{module}')
+        repo.git.pull()
+
+        print("Exporting Dockerfile for current pipeline module")
+        # First step, creating the Dockerfile for that particular revision of the
+        # git repository
+        dockerfile=f"""# Ceci n'est pas un Dockerfile
+# Created on {datetime.datetime.now()}
+FROM {CECI_CENTRAL_DOCKER}
+MAINTAINER {user} <{email}>
+
+RUN pip install git+{remote}@{hexsha}
+        """
+        with open(f'{tmpdir}/tools/{module}.docker', 'w') as f:
+            f.write(dockerfile)
+        repo.git.add(f'{tmpdir}/tools/{module}.docker')
+
+        # Second step, export each pipeline element as a CWL tool
+        __import__(module)
+        print("Exporting CWL description of each pipeline stage")
+        for k in PipelineStage.pipeline_stages:
+            tool = PipelineStage.pipeline_stages[k][0].generate_cwl()
+            tool.export(f'{tmpdir}/tools/{k}.cwl')
+            repo.git.add(f'{tmpdir}/tools/{k}.cwl')
+
+        # Now commit these changes and push them to ceci-central on a new branch
+        # TODO: Check for need to commit
+        repo.git.commit('-m "Modifies pipeline elements"')
+        repo.git.push("origin", f"{module}")
+
+        # Create pull request
+        repo.git.request_pull("master", CECI_CENTRAL_REPO, f'{module}')
+
 def main():
     args = parser.parse_args()
-    if args.export_cwl is not None:
-        export_cwl(args)
-    else:
-        run(args.pipeline_config)
+    push_module(args)
+    # if args.export_cwl is not None:
+    #     export_cwl(args)
+    # else:
+    #     run(args.pipeline_config)
 
 if __name__ == '__main__':
     main()
