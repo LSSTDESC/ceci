a4ce77feffcb38bf158b1fe0cf5fd6954a0e96e8
joezuntz <joezuntz@googlemail.com>
2019-06-25 07:13:50 -0700

    towards working cori batch site

--- ceci/sites/cori.py
+++ ceci/sites/cori.py
@@ -4,19 +4,14 @@ from parsl.executors import HighThroughputExecutor
 from parsl.providers import SlurmProvider
 
 def activate(queue, max_slurm_jobs, setup_script, cpu_type):
+    
     provider=SlurmProvider(
          partition=queue,  # Replace with partition name
          min_blocks=0, # one slurm job to start with
-         max_blocks=max_blocks, # one slurm job to start with
+         max_blocks=max_slurm_jobs, # one slurm job to start with
          scheduler_options=f"#SBATCH --constraint={cpu_type}",
          worker_init=f'source {setup_script}',
     )
-    #node_per_block = total_nodes_used
-    #max_blocks = 1
-    #init_blocks = 0
-    #min_blocks = 0
-    #launcher = SingleNodeLauncher()
-    #max_workers = total_mpi_applications
 
     executor = HighThroughputExecutor(
              label="cori",
@@ -24,8 +19,8 @@ def activate(queue, max_slurm_jobs, setup_script, cpu_type):
              provider=provider,
     )
 
-
-    config = Config(executors=[executor])
+    executors = [executor]
+    config = Config(executors=executors)
     parsl.load(config)
 
 
