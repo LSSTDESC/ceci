feb1a4faa72abc52db620d35c4ed8edbca5dcbe4
EiffL <fr.eiffel@gmail.com>
2020-05-28 20:05:11 +0200

    undo this mess I've made

--- ceci/__init__.py
+++ ceci/__init__.py
@@ -1,3 +1,9 @@
 from .stage import PipelineStage
 from .pipeline import Pipeline, MiniPipeline, ParslPipeline, DryRunPipeline
-from .version import __version__
+￼
+￼try:
+    ￼from pkg_resources import get_distribution
+￼    __version__ = get_distribution(__name__).version
+￼except:
+￼    # package is not installed, too bad, no version
+￼    pass
