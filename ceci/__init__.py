c1bf92995ddf9f3e41764c1217eaf250b553713a
EiffL <fr.eiffel@gmail.com>
2020-05-28 20:06:36 +0200

    fix small issue

--- ceci/__init__.py
+++ ceci/__init__.py
@@ -1,9 +1,10 @@
 from .stage import PipelineStage
 from .pipeline import Pipeline, MiniPipeline, ParslPipeline, DryRunPipeline
-￼
-￼try:
-    ￼from pkg_resources import get_distribution
-￼    __version__ = get_distribution(__name__).version
-￼except:
-￼    # package is not installed, too bad, no version
-￼    pass
+from pkg_resources import DistributionNotFound
+from pkg_resources import get_distribution
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
