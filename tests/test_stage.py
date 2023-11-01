757fb6ff90d16e52a6058370d4bee754e1b3ee4c
Joe Zuntz <joezuntz@googlemail.com>
2023-11-01 14:17:51 +0000

    another fix to test

--- tests/test_stage.py
+++ tests/test_stage.py
@@ -156,11 +156,8 @@ def test_parameter():
     class AbstractStage(PipelineStage):
         name="AbstractStage"
         config_options = None
-        inputs = []
-        outputs = []
 
-    abstract_stage = AbstractStage.make_stage()
-    abstract_stage.describe_configuration()
+    AbstractStage.describe_configuration()
 
 
     # This one should not work
